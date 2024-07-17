7b7048582002b218457c3b58ecb7dbf730916616
heavyrisem <heavyrisem@gmail.com>
2024-07-17 19:42:07 +0900

    ingress controller

--- master.sh
+++ master.sh
@@ -1,1 +1,8 @@
 kubeadm init --config kubernetes.yaml
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# CNI
+# kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
